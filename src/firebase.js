b0636ed840702aabbeb39a9ed560ed667fcb234f
Blairsa <135337930+Blairsa@users.noreply.github.com>
2026-08-25 08:43:19 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,25 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDYj5v8v8j6o3eYwZoTJoX0b-8ixu49Zx8",
+  authDomain: "mappin-14d4d.firebaseapp.com",
+  projectId: "mappin-14d4d",
+  storageBucket: "mappin-14d4d.firebasestorage.app",
+  messagingSenderId: "638312610226",
+  appId: "1:638312610226:web:e97242247a34f273b24cab",
+  measurementId: "G-XCCYR413C9"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+export const analytics = getAnalytics(app);
